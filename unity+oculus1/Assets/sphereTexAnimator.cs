14ae195dfc3b0738fea95478f7c18d9118d749c0
Ben Van Citters <van.citters.b@gmail.com>
2014-02-25 09:47:01 -0800

    initial commit

---
+++ unity+oculus1/Assets/sphereTexAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphereTexAnimator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		 
+		Vector2 offset = new Vector2 (0,Time.timeSinceLevelLoad);
+		renderer.material.SetTextureOffset ("_MainTex", offset);
+//		gameObject.renderer.normalMap.offset.x = time;
+//		time;
+	}
+}
